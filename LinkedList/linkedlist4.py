e2ab6a294b1afb0e88afe47294221ea5786b2b68
Madhurendra1311 <kmadhu1311@gmail.com>
2021-05-12 16:52:13 +0530

    linked_list_coding

--- LinkedList/linkedlist4.py
+++ LinkedList/linkedlist4.py
@@ -6,7 +6,7 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
-
+# 1)
      #print the linked_list   
 
     def print(self):
@@ -22,6 +22,7 @@ class LinkedList:
             iteration = iteration.next
         print(linkedStr)
 
+# 2)
     # length of the linked_list   
 
     def get_length(self):                  
@@ -35,10 +36,12 @@ class LinkedList:
 
      #insert the node at beginning   
 
+# 3)
     def insert_at_begining(self, data):
         node = Node(data, self.head)
         self.head = node
 
+# 4)
     def insert_at_end(self, data):
         if self.head is None:                  # if linked_list is blank
             self.head = Node(data, None)
@@ -51,6 +54,7 @@ class LinkedList:
 
         iteration.next = Node(data, None)
 
+# 5)
     # insert the node at middle of the linked_list
 
     def insert_at(self, index, data):
@@ -72,6 +76,7 @@ class LinkedList:
             iteration = iteration.next
             count += 1
 
+# 6)
     #delete the node from the linked_list        
 
     def remove_at(self, index):
@@ -92,11 +97,49 @@ class LinkedList:
             iteration = iteration.next
             count+=1
 
+# 7)
+    #add multiple values at the end of the node
     def insert_values(self, data_list):
         self.head = None
         for data in data_list:
             self.insert_at_end(data)
 
+# 8)
+    def insert_after_value(self, data_after, data_to_insert):
+        # Search for first occurance of data_after value in linked list
+        if self.head is None:
+            return
+
+        if self.head.data==data_after:
+            self.head.next = Node(data_to_insert,self.head.next)
+            return
+        # Now insert data_to_insert after data_after node
+        itr = self.head
+        while itr:
+            if itr.data == data_after:
+                itr.next = Node(data_to_insert, itr.next)
+                break
+
+            itr = itr.next
+
+# 9)
+    def remove_by_value(self, data):
+        # Remove first node that contains data
+        if self.head is None:
+            return
+
+        if self.head.data == data:
+            self.head = self.head.next
+            return
+
+        itr = self.head
+        while itr.next:
+            if itr.next.data == data:
+                itr.next = itr.next.next
+                break
+            itr = itr.next
+
+# 10)
     def reverseList(list):
         # initialize variables
         previous = None         # `previous` initially points to None
@@ -114,6 +157,7 @@ class LinkedList:
         list.head = previous
 
 
+
 if __name__ == '__main__':
     ll = LinkedList()
     # ll.insert_at_begining(5)
@@ -121,7 +165,9 @@ if __name__ == '__main__':
     ll.insert_values(["banana","mango","grapes","orange"])
     ll.insert_at(1,"tomato")
     ll.insert_at(2,"jackFruit")
-    ll.reverseList()
+    ll.insert_after_value("mango","apple")
+    ll.remove_by_value("orange")
+    # ll.reverseList()
     # ll.remove_at(2)
     # ll.print()
 
