ffb5f5dd5ad77f5fedd1c9b43d80ae70d7397454
Madhurendra1311 <kmadhu1311@gmail.com>
2021-05-18 12:18:50 +0530

    double_linkedlist

--- LinkedList/double_linkedlist.py
+++ LinkedList/double_linkedlist.py
@@ -22,18 +22,44 @@ class DoublyLinkedList:
             cur.next = new_node
             new_node.prev = cur
             new_node.next = None
-    # prepend the element
-    # def prepend(self, data):
-    #     if self.head is None:
-    #         new_node = Node(data)
-    #         new_node.prev = None 
-    #         self.head = new_node
-    #     else:
-    #         new_node = Node(data)
-    #         self.head.prev = new_node
-    #         new_node.next = self.head
-    #         self.head = new_node
-    #         new_node.prev = None
+    #prepend the element
+    def prepend(self, data):
+        if self.head is None:
+            new_node = Node(data)
+            new_node.prev = None 
+            self.head = new_node
+        else:
+            new_node = Node(data)
+            self.head.prev = new_node
+            new_node.next = self.head
+            self.head = new_node
+            new_node.prev = None
+    #add element after node
+    def add_after_node(self, key, data):
+        cur = self.head
+        while cur:
+            if cur.next is None and cur.data == key:
+                self.append(data)
+            elif cur.data == key:
+                new_node = Node(data)
+                nxt = cur.next 
+                cur.next = new_node
+                new_node.next = nxt
+                nxt.prev = new_node
+            cur = cur.next
+    #add element before node
+    def add_before_node(self, key, data):
+        cur = self.head 
+        while cur:
+            if cur.prev is None and cur.data == key:
+                self.prepend(data)
+            elif cur.data == key:
+                new_node = Node(data)
+                prev = cur.prev
+                prev.next = new_node
+                cur.prev = new_node
+                new_node.next = cur
+            cur = cur.next
 
     def print_list(self):
         cur = self.head
@@ -43,11 +69,13 @@ class DoublyLinkedList:
 
 if __name__ == "__main__":
     dllist = DoublyLinkedList()
-    # dllist.prepend(0)
+    dllist.prepend(0)
     dllist.append(1)
     dllist.append(2)
     dllist.append(3)
     dllist.append(4)
-    # dllist.prepend(5)
+    dllist.prepend(5)
+    dllist.add_after_node(3,6)
+    dllist.add_before_node(4,9)
 
     dllist.print_list()
