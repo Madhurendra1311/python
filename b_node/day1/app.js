2c62197316c783d5f9faf5912c33cd58512024eb
Madhurendra1311 <kmadhu1311@gmail.com>
2021-06-18 14:21:53 +0530

    backend_api

--- b_node/day1/app.js
+++ b_node/day1/app.js
@@ -1,61 +1,73 @@
 const express = require('express')
 const app = express()
 
-let users = [
+app.use(express.json())
+
+let books = [
     {
-        username: "ram",
-        email: "ram@gmail.com",
-        age: 20
+        title: "harry potter",
+        author: "JK Rowling",
+        year: 2001
     }
-];
+]
 
-app.get('/getuser/:username', (req, res) => {
-    const username = req.params.username
-    const getUser = users.find(user => {
-        return (user.username === username)
+app.get('/book', (req, res) => {
+    const title = req.body.title
+    console.log(title);
+    const getBooks = books.find(book => {
+        return (book.title === title)
     })
-    if (getUser) {
-        res.status(200).json(getUser)
+    if (getBooks) {
+        res.status(200).json(getBooks)
     }
     else {
         res.status(404).json({ error: "Erorr Fetching the User" })
     }
 })
 
-app.post('/adduser', (req, res) => {
-    const username = req.query.username
-    const email = req.query.email
-    const age = req.query.age
-    const user = { username, email, age }
-    users.push(user);
-    res.status(200).json({ message: "Student Added Successfully" })
+app.post('/create', (req, res) => {
+    const title = req.body.title
+    const author = req.body.author
+    const year = req.body.year
+    const book = { title, author, year }
+    books.push(book)
+    if (res.status(200)) {
+        res.json({ status: "success", message: `${title} has been added to the array` })
+    } else {
+        res.status(404).json({ error: "No such book found" })
+    }
 })
 
-app.delete('/deleteuser/:username', (req, res) => {
-    const username = req.params.username
-    const userIndex = users.findIndex(user => {
-        return (user.username === username)
+app.delete('/delete', (req, res) => {
+    const userbook = req.body.title
+    const bookIndex = books.findIndex(item => {
+        return (item.title === userbook)
     })
-    if (userIndex >= 0) {
-        users.splice(userIndex, 1);
-        res.status(200).json({ message: "Delete Successful" })
+    if (bookIndex >= 0) {
+        books.splice(bookIndex, 1);
+        res.status(200).json({ status: "success", message: `${userbook} has been removed from the array` })
     }
     else {
-        res.status(404).json({ error: "Erorr Fetching the User" })
+        res.status(404).json({ error: "No such book found" })
     }
 })
 
-app.get('/*', (req, res) => {
-    res.status(404).send('Error!')
-})
-app.post('/*', (req, res) => {
-    res.status(404).send('Error!')
-})
-app.put('/*', (req, res) => {
-    res.status(404).send('Error!')
-})
+app.put('/edityear', (req, res) => {
+    const index = books.findIndex(item => {
+        return (item.title == req.body.book)
+    })
+    console.log(index);
 
 
+    if (index >= 0) {
+        const updatedYear = books[index];
+        updatedYear.year = req.body.year
+        res.status(200).json(updatedYear)
+    }
+    else {
+        res.status(404).json({ error: "Updation of year failed" })
+    }
+})
 
 app.listen(5000, () => {
     console.log('The server is up and running')
